0871e92731f330ad9525fb1f37e6c396e4acefd3
Your-Full-Name <33285905+piaobodewu@users.noreply.github.com>
2018-07-20 17:19:17 +0800

    home
    home

--- pages/home/home.js
+++ pages/home/home.js
@@ -7,15 +7,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-    imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
-    ],
+    imgUrls: [],
+      // 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      // 'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      // 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      // 'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      // 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      // 'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      // 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
     indicatorDots: true,
     autoplay: true,
     interval: 5000,
@@ -46,7 +45,7 @@ Page({
       display_car_b: 'none',
       mode_per_bgcolor: '#f6b26b',
       mode_car_bgcolor: '#E9E7E7'
-    })
+    });
   },
   mode_car_tap: function(){
     this.setData({
@@ -62,7 +61,27 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this
+    var fetch
+    //抓取主页轮播图
+    wx.request({
+      url: 'https://www.acampus.cn/get_carousel',
+      method: 'POST',
+      success: function (result) {
+        fetch = result.data.msg
+        console.log(fetch)
+        that.setData({
+          imgUrls: fetch
+        })
+      },
+      fail: res => {
+        wx.showToast({
+          title: '网络不好哟',
+          image: '/image/wrong.png',
+          duration: 3000
+        })
+      }
+    })
   },
 
   /**
