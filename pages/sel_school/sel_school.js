d74c3dc9edd85be33d40f40316137e1d9b829fcb
Your-Full-Name <33285905+piaobodewu@users.noreply.github.com>
2018-08-29 17:43:55 +0800

    选择学校js实现完成
    选择学校js实现完成

--- pages/sel_school/sel_school.js
+++ pages/sel_school/sel_school.js
@@ -1,13 +1,81 @@
 // pages/sel_school/sel_school.js
 var that
 var list = []
+var user_province
+var user_school
+var user_province_id
+var user_school_id
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    user_province: '',
+    user_school: ''
+  },
+  //下拉出现和隐藏
+  pull_school: function (e) {
+    //历史订单fetch 
+    var that = this
+    var temp
+    var fetch
+    var user_province_id = 12
+    wx.request({
+      url: 'https://www.acampus.cn/pull_school',
+      method: 'POST',
+      header: {
+        'content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json'
+      },
+      data: {
+        user_province_id: user_province_id,
+      },
+      success: result => {
+        if (result.data.result == 'success') {
+          fetch = result.data.msg;
+          user_school_id = fetch;
+          user_school = fetch[fetch.length-1];
+          fetch = user_school;
+          that.setData({
+            array: fetch,
+            hasHistoryOrder: true
+          })
+          wx.showActionSheet({
+            itemList: fetch,
+            success: function (res) {
+              user_school_id = user_school_id[res.tapIndex];
+              console.log(user_school_id);
+            },
+            fail: function (res) {
+              console.log(res.errMsg)
+            }
+          })
+        } else if (result.data.msg == 'session已过期') {
+          var temp = wxlogin.wxLoginAgain()
+          wx.showToast({
+            title: '请重试',
+            image: '/image/wrong.png',
+            duration: 3000
+          })
+        } else if (result.data.msg == '无历史订单') {
+          //do nothing
+        } else {
+          wx.showToast({
+            title: result.data.msg,
+            image: '/image/wrong.png',
+            duration: 3000
+          })
+        }
+      },
+      fail: res => {
+        wx.showToast({
+          title: '网络不好哟',
+          image: '/image/wrong.png',
+          duration: 3000
+        })
+      }
+    })
   },
 
   /**
